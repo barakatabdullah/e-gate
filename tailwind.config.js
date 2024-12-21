ae89b03e0bc71689c2d4e1ee75d1221f27679977
Barakat <abdallah771363885@gmail.com>
2024-12-21 12:09:16 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#1B8354',
+        secondary:'#B67F59',
+        tertiary:'#2A4777',
+      }
+    },
+    
+  },
+  plugins: [],
+}
+
